d3326944aee96c052cf4a12c5d54c21122ad9b4e
Krishnaarunangsu <arunangsutech@gmail.com>
2019-04-04 14:43:00 +0530

    first commit

--- src/main/java/com/krishna/test/MyArrayList.java
+++ src/main/java/com/krishna/test/MyArrayList.java
@@ -92,6 +92,7 @@ public class MyArrayList {
 		list.add(5);
 		list.add(6);
 		list.add(7);
+		list.add(8);
 
 		System.out.println("Initial ArrayList: " + list + "| size: " + list.size());
 		// System.out.println("Get index 2: " + list.get(2));
